93b5d3941385467c57c2c9bfc0f304e838f3711c
grasielaGomes <produtoradama@gmail.com>
2023-11-07 20:05:35 -0300

    refactor: change App fonts and style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+
+import { colors } from './src/styles/tokens'
+import { useFonts } from 'expo-font'
+import { ToDoList } from './src/pages/ToDoList'
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+  })
+
+  if (!fontsLoaded && !fontError) {
+    return null
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" translucent />
+      <ToDoList />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.gray600,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
-});
+})
