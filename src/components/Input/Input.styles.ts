2df32225c0ea317c06381347a7a13a1a19b75eb1
grasielaGomes <produtoradama@gmail.com>
2023-11-07 20:12:03 -0300

    refactor: change styles

--- src/components/Input/Input.styles.ts
+++ src/components/Input/Input.styles.ts
@@ -4,12 +4,13 @@ import { colors } from '../../styles/tokens'
 export const styles = (hasFocus: boolean) =>
   StyleSheet.create({
     input: {
+      flex: 1,
+      height: 54,
       backgroundColor: colors.gray500,
       borderRadius: 8,
       padding: 16,
       borderWidth: 1,
       borderColor: hasFocus ? colors.purpleDark : colors.gray700,
       color: colors.gray100,
-      width: '100%',
     },
   })
